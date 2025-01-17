861245f9632953e70786f37418553f6e994708aa
NushK650 <Nush8653@gmail.com>
2025-01-17 00:23:56 -0800

    popup

---
+++ Scripts/app.js
@@ -0,0 +1,7 @@
+const rulesBtn = document.getElementById('rulesBtn');
+const rulesPopup = document.getElementById('rulesPopup');
+
+rulesBtn.addEventListener('click', function() {
+    rulesPopup.classList.remove('hidden');
+  });
+
